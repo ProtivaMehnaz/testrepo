734ee26c50614f638624379b0b1081bbaa82316d
Mehnaz Afrose <115811790+ProtivaMehnaz@users.noreply.github.com>
2024-05-20 13:22:07 -0500

    Add files via upload

---
+++ DataScienceEcoSystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - Commonly used libraries for Data Science
+# - Demonstrate basic arithmetic operations in Python
+# - Convert units of time from minutes to hours
+# 
+# ## Author
+# Mehnaz Afrose
+
+# Some of the popular languages that Data Scientists use are: <br>
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. TensorFlow
+# 5. Pandas
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. NumPy: Fundamental package for numerical computing in Python.
+# 2. Pandas: Provides data structures and data analysis tools.
+# 3. Matplotlib: Plotting library for creating static, animated, and interactive visualizations.
+# 4. SciPy: Library used for scientific and technical computing.
+# 5. scikit-learn: Machine learning library that provides simple and efficient tools for data mining and data analysis.
+# 6. TensorFlow: Open-source library for machine learning and artificial intelligence.
+# 7. Keras: High-level neural networks API, running on top of TensorFlow.
+# 8. Seaborn: Statistical data visualization library based on Matplotlib.
+# 9. Statsmodels: Provides classes and functions for the estimation of statistical models.
+# 10. NLTK (Natural Language Toolkit): Platform for building Python programs to work with human language data.
+# 
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | Apache Zeppelin    |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[7]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+output = (3 * 4) + 5
+output
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minute = 200
+hours = minute/60
+hours
+
